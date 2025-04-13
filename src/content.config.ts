7b1d5aef706d306bb6954ffd63b3648e88c1c74b
Jonas Amsen-Wallander <jonas@lvira.dev>
2025-04-13 21:56:40 +0200

    add content collection connection

---
+++ src/content.config.ts
@@ -0,0 +1,24 @@
+﻿import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+let recipes = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: './src/recipes' }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		tags: z.array(z.string()),
+	})
+});
+
+let preparations = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: './src/preparations' }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+	})
+});
+
+export let collections = {
+	recipes,
+	preparations
+}

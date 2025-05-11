d03d2423e4a89bdcfb78a1b2a26b31a505c523c9
Jonas Amsen-Wallander <jonas@lvira.dev>
2025-05-11 20:52:44 +0200

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [svelte()]
 });
